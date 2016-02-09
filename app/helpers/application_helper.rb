9bcb85cecb96a51f6d686716de2fa0ed0a5a1df1
Loretta Cain <locacodes@gmail.com>
2016-02-09 16:11:03 +1100

    styling and deleted css-json.js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,10 +9,15 @@ module ApplicationHelper
     if user_signed_in?
       nav += '<li class="nav button" id="signout">' + "Log out #{ current_user.email }" + '</li>'
     else
+      nav += '<div class="innerLogin1">'
       nav += '<nav class="nav">Email:</nav>' + '<input type="text" id="email" class="nav">'
       nav += '<nav class="nav">Password:</nav>' + '<input type="password" id="password" class="nav">'
+      nav += '</div>'
+      nav += '<div class="innerLogin2">'
       nav += '<li class="nav button" id="signin">' + "Sign in" + '</li>'
+      nav += '<span>or</span>'
       nav += '<li class="nav button" id="signup">' + "Sign up" + '</li>' 
+      nav += '</div>'
     end
     nav += '</div>'
 
